8a48e05dfbd3b09ce8713e879ccd1fd4360ff40e
Jono Wenger <jono@elixxir.io>
2022-08-22 16:53:07 -0700

    Implement build constraints when compiling for WebAssembly

---
+++ connect/connectionType.go
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
+//go:build !js || !wasm
+// +build !js !wasm
+
+package connect
+
+// GetDefaultConnectionType returns Grpc as the default connection type when
+// compiling for all architectures except WebAssembly.
+func GetDefaultConnectionType() ConnectionType {
+	return Grpc
+}

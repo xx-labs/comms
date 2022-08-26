4f16fad11704357d59ca251dbea9d5dfa195528a
Jono Wenger <jono@elixxir.io>
2022-08-26 10:21:25 -0700

    Update compiled proto files to new protoc-gen-go repository; protoc: v3.15.6, protoc-gen-go: v1.27.1, protoc-gen-go-grpc: v1.2.0

--- interconnect/handler.go
+++ interconnect/handler.go
@@ -67,6 +67,7 @@ func StartCMixInterconnect(id *id.ID, port string, handler ServerHandler,
 type CMixServer struct {
 	*connect.ProtoComms
 	handler ServerHandler
+	*UnimplementedInterconnectServer
 }
 
 // Handler for CMix -> consensus

ec192ff99f5e7cc8a93e8a1f4fffcc323fcca8b5
Jono Wenger <jono@elixxir.io>
2022-09-13 13:33:05 -0700

    Fix tests

--- connect/connection_test.go
+++ connect/connection_test.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"gitlab.com/xx_network/comms/connect/token"
 	pb "gitlab.com/xx_network/comms/messages"
+	"gitlab.com/xx_network/comms/testkeys"
 	"gitlab.com/xx_network/crypto/csprng"
+	"gitlab.com/xx_network/crypto/signature/rsa"
 	"gitlab.com/xx_network/primitives/id"
+	"gitlab.com/xx_network/primitives/utils"
 	"google.golang.org/grpc"
 	"net"
 	"testing"
@@ -77,77 +80,77 @@ func TestWebConnection(t *testing.T) {
 }
 
 // TODO: Re-enable once HTTPS is supported
-// func TestWebConnection_TLS(t *testing.T) {
-// 	addr := "0.0.0.0:11421"
-//
-// 	certBytes, err := utils.ReadFile(testkeys.GetNodeCertPath())
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	keyBytes, err := utils.ReadFile(testkeys.GetNodeKeyPath())
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	lis, err := net.Listen("tcp", addr)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	rng := csprng.NewSystemRNG()
-// 	hostId, err := id.NewRandomID(rng, id.User)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	hostParams := GetDefaultHostParams()
-// 	TestingOnlyDisableTLS = true
-// 	hostParams.ConnectionType = Web
-//
-// 	h, err := newHost(hostId, addr, certBytes, hostParams)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	s := grpc.NewServer()
-// 	pb.RegisterGenericServer(s, &TestGenericServer{})
-//
-// 	pk, err := rsa.LoadPrivateKeyFromPem(keyBytes)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	salt := make([]byte, 8)
-// 	_, err = rng.Read(salt)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	go func() {
-// 		pc := ProtoComms{
-// 			networkId:   id.NewIdFromString("zezima", id.User, t),
-// 			privateKey:  pk,
-// 			disableAuth: false,
-// 			tokens:      token.NewMap(),
-// 			Manager:     newManager(),
-// 			netListener: lis,
-// 			grpcServer:  s,
-// 			pubKeyPem:   certBytes,
-// 			salt:        nil,
-// 		}
-// 		pc.ServeWithWeb()
-// 	}()
-// 	time.Sleep(time.Second * 5)
-//
-// 	err = h.connect()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	ctx, cancel := h.GetMessagingContext()
-// 	defer cancel()
-//
-// 	resp := &pb.Ack{}
-// 	err = h.connection.GetWebConn().Invoke(ctx, "/messages.Generic/AuthenticateToken", &pb.AuthenticatedMessage{}, resp)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
+func TestWebConnection_TLS(t *testing.T) {
+	addr := "0.0.0.0:11421"
+
+	certBytes, err := utils.ReadFile(testkeys.GetNodeCertPath())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keyBytes, err := utils.ReadFile(testkeys.GetNodeKeyPath())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rng := csprng.NewSystemRNG()
+	hostId, err := id.NewRandomID(rng, id.User)
+	if err != nil {
+		t.Fatal(err)
+	}
+	hostParams := GetDefaultHostParams()
+	TestingOnlyDisableTLS = true
+	hostParams.ConnectionType = Web
+
+	h, err := newHost(hostId, addr, certBytes, hostParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGenericServer(s, &TestGenericServer{})
+
+	pk, err := rsa.LoadPrivateKeyFromPem(keyBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	salt := make([]byte, 8)
+	_, err = rng.Read(salt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		pc := ProtoComms{
+			networkId:   id.NewIdFromString("zezima", id.User, t),
+			privateKey:  pk,
+			disableAuth: false,
+			tokens:      token.NewMap(),
+			Manager:     newManager(),
+			netListener: lis,
+			grpcServer:  s,
+			pubKeyPem:   certBytes,
+			salt:        nil,
+		}
+		pc.ServeWithWeb()
+	}()
+	time.Sleep(time.Second * 5)
+
+	err = h.connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := h.GetMessagingContext()
+	defer cancel()
+
+	resp := &pb.Ack{}
+	err = h.connection.GetWebConn().Invoke(ctx, "/messages.Generic/AuthenticateToken", &pb.AuthenticatedMessage{}, resp)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
